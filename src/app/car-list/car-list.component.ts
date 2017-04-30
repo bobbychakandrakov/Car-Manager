fb6a00cc5384fbbc8a4793ce715326fd527e509d
bobilincho <bobilincho@gmail.com>
2017-04-30 16:50:08 +0300

    Car delete modal

--- src/app/car-list/car-list.component.ts
+++ src/app/car-list/car-list.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 
 import { Car } from '../car';
 import { CarDataService } from '../car-data.service';
+import {NgbModal, ModalDismissReasons} from '@ng-bootstrap/ng-bootstrap';
+
 
 @Component({
   selector: 'app-car-list',
@@ -9,8 +11,33 @@ import { CarDataService } from '../car-data.service';
   styleUrls: ['./car-list.component.css']
 })
 export class CarListComponent implements OnInit {
+  closeResult: string;
+  cars: Car[];
 
-  constructor(private carService: CarDataService) { }
+  constructor(private carService: CarDataService, private modalService: NgbModal) { 
+    this.cars = carService.getAll() || [];
+  }
+
+  open(content, id) {
+    this.modalService.open(content).result.then((result) => {
+      if (result == 'Confirm click'){
+        this.carService.removeCar(id);
+      }
+      this.closeResult = `Closed with: ${result}`;
+    }, (reason) => {
+      this.closeResult = `Dismissed ${this.getDismissReason(reason)}`;
+    });
+  }
+
+  private getDismissReason(reason: any): string {
+    if (reason === ModalDismissReasons.ESC) {
+      return 'by pressing ESC';
+    } else if (reason === ModalDismissReasons.BACKDROP_CLICK) {
+      return 'by clicking on a backdrop';
+    } else {
+      return  `with: ${reason}`;
+    }
+  }
 
   ngOnInit() {
     console.log(this.carService.getAll());
