8087a1d26a6743093fbbfb899cbe369cbc9f573f
bobilincho <bobilincho@gmail.com>
2017-04-30 15:28:39 +0300

    Create Car form

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,17 +2,29 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { RouterModule, Routes } from '@angular/router';
 
 import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
 
 
 import { AppComponent } from './app.component';
+import { CarListComponent } from './car-list/car-list.component';
+import { CarFormComponent } from './car-form/car-form.component';
+
+
+const appRoutes: Routes = [
+  { path: '', component: CarListComponent },
+  { path: 'create', component: CarFormComponent }
+  ];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CarListComponent,
+    CarFormComponent
   ],
   imports: [
+    RouterModule.forRoot(appRoutes),
     BrowserModule,
     FormsModule,
     HttpModule,
